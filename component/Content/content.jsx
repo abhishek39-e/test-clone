f732487b0a41bff121f88223cfc4cf6b0a67c936
red arch <abhishekkaush39@gmail.com>
2026-02-15 21:30:09 +0530

    update

---
+++ component/Content/content.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const content = () => {
+  return (
+    <div>
+      <div className='xclone-content-post '>
+        <div className='xclone-post-form-container p-6 border border-sky-400 border-solid'>
+          <div className='post-show-profile'></div>
+          <div className='post-from'>
+            <input
+              type='text'
+              placeholder='What is happening?!'
+              className='text-gray-200 border-0 outline-0'
+            />
+            <input
+              type='submit'
+              value='post'
+              className='bg-sky-500 m-2 px-5 py-2 border rounded-4xl border-0 cursor-pointer'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default content;
--- component/interface_data/home.jsx
+++ component/interface_data/home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import navbar from '../Navbar/navbar';
+import Navbar from '../Navbar/navbar';
+import Content from '../Content/content';
 
 const Home = () => {
   return (
     <>
-      <div className='x_clone_container-x1 flex h-screen w-full justify-around'>
-        <div className='x_clone_nav '>
-          <navbar />
+      <div className='x_clone_container-x1 flex h-screen w-full justify-around bg-black text-white'>
+        <div className='x_clone_nav '>{/* <Navbar /> */}</div>
+        <div className='xclone_content'>
+          <Content />
         </div>
-        <div className='xclone_content'>Content</div>
         <div className='xclone_following'>Following</div>
       </div>
     </>
