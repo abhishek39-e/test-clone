55e2b46a6c3d891b1eb70b856cf1dcc805781b2c
red arch <abhishekkaush39@gmail.com>
2026-02-20 21:38:41 +0530

    update

--- component/Navbar/navbar.jsx
+++ component/Navbar/navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='text-3xl font-black text-white'>𝕏</div>
       </div>
 
-      {/* Navigation Mend */}
+      {/* Navigation Men */}
       <nav className='flex-1 space-y-4'>
         {menuItems.map((item) => {
           const Icon = item.icon;
