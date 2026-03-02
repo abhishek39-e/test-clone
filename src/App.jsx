22f88af0fea0a6d34d19e89b2e3051aebd817733
red arch <abhishekkaush39@gmail.com>
2026-03-02 21:21:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     //route
     <>
-      {/* <Login /> */}
+      {/* Login routes */}
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/loginpage' element={<Loginpage />} />
